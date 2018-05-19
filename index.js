5168a2feed834058744a3961730b87d239f2c953
SUE HELLEN DEL CARPIO BELLIDO VARGAS <sueDCBV@gmail.com>
2018-05-18 21:59:10 -0700

    upd search

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Get references to the tbody element, input field and button
 var $tbody = document.querySelector("tbody");
-var $stateInput = document.querySelector("#state");
-var $searchBtn = document.querySelector("#search");
+var $stateInput = document.querySelector("#txt_state");
+var $searchBtn = document.querySelector("#btn_filter");
 
 // Add an event listener to the searchButton, call handleSearchButtonClick when clicked
-// $searchBtn.addEventListener("click", handleSearchButtonClick);
+$searchBtn.addEventListener("click", handleSearchButtonClick);
 
 
 $(document).ready(function(){
@@ -13,39 +13,42 @@ $(document).ready(function(){
 
 
 // Set filteredAddresses to addressData initially
-var filteredAddresses = dataSet;
+var filteredData = dataSet;
+
+
 
 // renderTable renders the filteredAddresses to the tbody
 function renderTable() {
   $tbody.innerHTML = "";
-  for (var i = 0; i < filteredAddresses.length; i++) {
+  for (var i = 0; i < filteredData.length; i++) {
     // Get get the current address object and its fields
-    var address = filteredAddresses[i];
-    var fields = Object.keys(address);
+    var ufo_sights = filteredData[i];
+    var fields = Object.keys(ufo_sights);
     // Create a new row in the tbody, set the index to be i + startingIndex
     var $row = $tbody.insertRow(i);
     for (var j = 0; j < fields.length; j++) {
       // For every field in the address object, create a new cell at set its inner text to be the current value at the current address's field
       var field = fields[j];
       var $cell = $row.insertCell(j);
-      $cell.innerText = address[field];
+      $cell.innerText = ufo_sights[field];
     }
   }
 }
 
-// function handleSearchButtonClick() {
-//   // Format the user's search by removing leading and trailing whitespace, lowercase the string
-//   var filterState = $stateInput.value.trim().toLowerCase();
+function handleSearchButtonClick() {
+  // Format the user's search by removing leading and trailing whitespace, lowercase the string
+  var filterState = $stateInput.value.trim().toLowerCase();
 
-//   // Set filteredAddresses to an array of all addresses whose "state" matches the filter
-//   filteredAddresses = dataSet.filter(function(address) {
-//     var addressState = address.state.toLowerCase();
+  // Set filteredAddresses to an array of all addresses whose "state" matches the filter
+  filteredData = dataSet.filter(function(ufo_sights) 
+  {
+    var sightsState = ufo_sights.state.toLowerCase();
+    // If true, add the address to the filteredAddresses, otherwise don't add it to filteredAddresses
+    return sightsState === filterState;
+  });
 
-//     // If true, add the address to the filteredAddresses, otherwise don't add it to filteredAddresses
-//     return addressState === filterState;
-//   });
-//   renderTable();
-// }
+  renderTable();
+}
 
 
 // Render the table for the first time on page load
